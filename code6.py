7bd7429730a861fbbdd91361daa4dfc90c138ca8
RelaxingLatte <phoenixteacup@outlook.com>
2023-01-22 12:59:13 -0800

    made some more code
    more advance subjects

---
+++ code6.py
@@ -0,0 +1,16 @@
+monthconversions = {
+    "Jan": "January",
+    "Feb": "Feburary",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print(monthconversions.get("Aug"))
---
+++ code8.py
@@ -0,0 +1,10 @@
+word = "banana"
+guess = ""
+
+while guess != word:
+    guess = input("Enter guess, keep going until i say it is correct ")
+    
+print("HOW!!!!!!!")
+print("fine im very pro at fortnite")
+
+bruh = input("Press enter to attempt it once again")
