b3e8474becc3223581827420a1b079e05a5cd127
RelaxingLatte <phoenixteacup@outlook.com>
2023-01-23 15:30:25 -0800

    edited some stuff

--- pro.py
+++ pro.py
@@ -9,11 +9,11 @@ operations = {
 def domath(left, operation, right):
     return operations[operation](int(left), int(right))
 
-num1 = input()
-op = input()
-num2 = input()
-print(domath(num1, op, num2)) #prints 150
+num1 = input("Enter a number please")
+op = input("And operator?")
+num2 = input("Lastly put another number in")
+print(domath(num1, op, num2)) # The Operation is pro
 
-
+print("Thanks for using the totally by me calculator!")
 
 reekid = input("please type anything to exit and try again!")
